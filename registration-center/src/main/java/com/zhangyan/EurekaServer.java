48e8735c06ace71fc29720000361a93952c36eec
张延17 <zhangyan17@baijiahulian.com>
2021-03-20 21:11:29 +0800

    注册中心

---
+++ registration-center/src/main/java/com/zhangyan/EurekaServer.java
@@ -0,0 +1,20 @@
+package com.zhangyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka注册中心
+ * @author zhonghuashishan
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer.class, args);
+	}
+	
+}
